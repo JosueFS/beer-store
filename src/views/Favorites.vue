287ce500289f6edc6665f681dad5d6dbb761e4de
JosueFS <josue.18rj@gmail.com>
2021-10-25 14:31:35 -0300

    feat: Favorites page ❤✅

--- src/views/Favorites.vue
+++ src/views/Favorites.vue
@@ -1,50 +1,62 @@
 <template>
-  <div>
-    <!-- <div class="event-header">
-      <span class="eyebrow">@{{ event.time }} on {{ event.date }}</span>
-      <h1 class="title">{{ event.title }}</h1>
-      <h5>Organized by {{ event.organizer }}</h5>
-      <h5>Category: {{ event.category }}</h5>
-    </div>
-    <BaseIcon name="map"><h2>Location</h2></BaseIcon>
-    <address>{{ event.location }}</address>
-    <h2>Beer details</h2>
-    <p>{{ event.description }}</p>
-    <h2>
-      Attendees
-      <span class="badge -fill-gradient">{{
-        event.attendees ? event.attendees.length : 0
-      }}</span>
-    </h2>
-    <ul class="list-group">
-      <li
-        v-for="(attendee, index) in event.attendees"
-        :key="index"
-        class="list-item"
-      >
-        <b>{{ attendee.name }}</b>
-      </li>
-    </ul> -->
-  </div>
+  <v-row class="flex-wrap">
+    <v-col
+      cols="4"
+      rows="12"
+      no-gutters
+      v-for="beer in favorites"
+      :key="beer.id"
+    >
+      <BeerCard :beer="beer" @update-favorites="updateFavorite">
+        {{ beer }}
+      </BeerCard>
+    </v-col>
+  </v-row>
 </template>
 
 <script>
-// import api from "@/services/api.js";
+import BeerCard from "@/components/BeerCard.vue";
 
 export default {
-  // props: ["id"],
+  components: {
+    BeerCard,
+  },
   data() {
     return {
-      event: {},
+      favorites: [],
     };
   },
-  // async created() {
-  //   try {
-  //     const response = await api.get(`events/${this.id}`);
-  //     this.event = response.data;
-  //   } catch (error) {
-  //     console.log(error);
-  //   }
-  // },
+  methods: {
+    updateFavorite(beer, action) {
+      switch (action) {
+        case "add":
+          this.favorites.push(beer);
+          localStorage.setItem(
+            "@BeerList:Favorites",
+            JSON.stringify(this.favorites)
+          );
+          break;
+        case "remove":
+          this.favorites = this.favorites.filter(
+            (favorite_beer) => favorite_beer.id !== beer.id
+          );
+          localStorage.setItem(
+            "@BeerList:Favorites",
+            JSON.stringify(this.favorites)
+          );
+          break;
+        default:
+          break;
+      }
+    },
+  },
+  created() {
+    try {
+      this.favorites =
+        JSON.parse(localStorage.getItem("@BeerList:Favorites")) || [];
+    } catch (error) {
+      console.log(error);
+    }
+  },
 };
 </script>
