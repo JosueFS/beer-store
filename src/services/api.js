e50469762b6c75c594912094da42d26ba3a9c131
Josué F. <45906601+JosueFS@users.noreply.github.com>
2026-03-01 00:05:57 -0300

    Change API base URL to new endpoint

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://api.punkapi.com/v2/',
+  baseURL: 'https://punkapi-alxiw.amvera.io/v3/',
   headers: {
     Accept: 'application/json',
     'Content-Type': 'application/json',
